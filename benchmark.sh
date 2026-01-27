d646e39c7ec48b5bb4dd3a56f1a75c75407353d4
wjr-timmers <wjr.timmers@outlook.com>
2026-01-27 16:40:07 +0100

    Troubleshooted slurm script

--- benchmark.sh
+++ benchmark.sh
@@ -3,17 +3,17 @@
 #SBATCH --output=logs/bench_%A_%a.out
 #SBATCH --error=logs/bench_%A_%a.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=60
 #SBATCH --time=01:00:00
 
+module load 2025
 module load Python/3.13.1-GCCcore-14.2.0
 
-cp $SLURM_SUBMIT_DIR/benchmark.py $TMPDIR/
-cp $SLURM_SUBMIT_DIR/sudoku.csv $TMPDIR/
+cp -r $SLURM_SUBMIT_DIR/* $TMPDIR/
 cd $TMPDIR
 
-CHUNK_SIZE=1000
-NUM_PUZZLES=CHUNK_SIZE*$SLURM_CPUS_PER_TASK
+CHUNK_SIZE=5000
+NUM_PUZZLES=$(( CHUNK_SIZE * SLURM_CPUS_PER_TASK ))
 
 echo "Starting benchmark: Total $NUM_PUZZLES puzzles using $SLURM_CPUS_PER_TASK workers."
 
